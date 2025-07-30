f4dcf2e8ffc4b331d0e8ce18a55eb0ea76f6634f
Grant Oganyan <grant1599@mail.ru>
2025-07-29 20:32:15 -0400

    chore: fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,8 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  {
-    ignores: ["**/dist/**"],
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
   },
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-    },
-  }
-);
+});
